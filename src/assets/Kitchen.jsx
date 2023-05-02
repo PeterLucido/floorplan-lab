1bd69e312d5646c9a9540b33bb85923b9987bfcd
Peter Lucido <petermlucido@gmail.com>
2023-05-01 21:02:11 -0500

    finish react floor plan

---
+++ src/assets/Kitchen.jsx
@@ -0,0 +1,13 @@
+import Sink from "./Sink"
+import Oven from "./Oven"
+
+const Kitchen = () => {
+  return (
+    <div>
+      <h2>Kitchen</h2>
+      <Oven />
+      <Sink />
+    </div>
+  )
+}
+export default Kitchen
